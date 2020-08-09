ccfe7fe9c31e3486ccda70ecdf9b005d89406b2e
BartlomiejBak <bakbartlomiej88>
2020-08-09 22:07:05 +0200

    added jdbc authentication

--- src/main/java/org/mavendemo/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/org/mavendemo/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,25 +1,27 @@
 package org.mavendemo.springsecurity.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
+
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    //add a reference to security data source
+    @Autowired
+    private DataSource securityDataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
-
-        auth.inMemoryAuthentication()
-                .withUser(userBuilder.username("john").password("qwerty").roles("EMPLOYEE"))
-                .withUser(userBuilder.username("merry").password("qwerty").roles("EMPLOYEE", "MANAGER"))
-                .withUser(userBuilder.username("pippin").password("qwerty").roles("EMPLOYEE", "ADMIN"));
+        //use jdbc authentication
+        auth.jdbcAuthentication().dataSource(securityDataSource);
     }
 
     @Override
